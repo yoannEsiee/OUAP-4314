b3aff176ef20059b47497ae3acc5a751b6760cce
Yoann GALLOCHAT <gallochy@pc5301-test.esiee.fr>
2018-03-15 11:27:44 +0100

    add ex2

--- Introduction/exercice2.py
+++ Introduction/exercice2.py
@@ -4,26 +4,26 @@ import re
 
 class req:
     def __init__(self):
-	self.timeout=10
-	self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 		(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        self.timeout=10
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 	  (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     def Connected(self,url):
-	response = requests.get(url, headers=self.headers, timeout=self.timeout)
+        response = requests.get(url, headers=self.headers, timeout=self.timeout)
 
-	x = self.delete_space(response.text)
-	x = self.delete_caractere(x)
-	return x
+        x = self.delete_space(response.text)
+        x = self.delete_caractere(x)
+        return x
 	
     def delete_space(self,code):
-	code = " ".join(code.split())
-	return code
+        code = " ".join(code.split())
+        return code
 	
     def delete_caractere(self,code):
 	#regex = [^A-Za-Z0-9]+
-	code = re.sub(code, "[^a-zA-Z0-9_]", "");
-	return code
+        code = re.sub( "[^a-zA-Z0-9_\s]", "", code);
+        return code
 	
 
 result=req()
-result.Connected(url="http://www.esiee.fr/")
+print(result.Connected(url="http://www.esiee.fr/"))
 
