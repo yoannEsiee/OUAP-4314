613eff49b69453cf1949d3d33849a66abd5daceb
Yoann GALLOCHAT <gallochy@pc5301-test.esiee.fr>
2018-03-22 10:36:40 +0100

    add exercice

--- Mongo/Exercice.py
+++ Mongo/Exercice.py
@@ -1,9 +1,27 @@
 from pymongo import MongoClient
+import pandas as pd
 
-client = MongoClient("http://127.0.0.1:27017")
+client = MongoClient()
+client = MongoClient("mongodb://localhost:27017")
 
-client.database_names()
+
+db = client.madatabase
+collection = db.macollection
+
+collection.find_one()
+
+df = pd.read_csv("data/ks-projects-201801-sample.csv") 
+
+#objects = df.to_json(orient="records") 
+
+collection.insert_many(df)
+
+#recupérer les 5 projets ayant reçu le plus de promesses de dons
+
+collection.find().sort("pledged",-1).limit(5)
 
 
-read.csv
+
+
+
 
